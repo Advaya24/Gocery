26c211f28e1aa5d5a684d0b8b5b534a5c484fb1d
Advaya24 <advaya24@yahoo.co.in>
2020-04-03 22:35:15 +0530

    Getting location as text input and displaying on new page

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
+import json
+
 from flask import Flask
 from flask import render_template
+from flask import request
+import requests
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
     content = {'text': "Hello from Flask"}
+
     return render_template('gocery/Index.html', content=content), 200
+
+
+@app.route('/', methods=['POST'])
+def got_location():
+    return request.form['location']
 
 
 if __name__ == '__main__':
