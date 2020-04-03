d03e06ef331b21b5a216c6b1c5d602741b2f4f14
Advaya24 <advaya24@yahoo.co.in>
2020-04-03 22:03:03 +0530

    Fixed internal server error

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     content = {'text': "Hello from Flask"}
-    return render_template('gocery/Index.html', content)
+    return render_template('gocery/Index.html'), content
 
 
 if __name__ == '__main__':
