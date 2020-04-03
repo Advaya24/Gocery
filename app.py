2ee139ad3979ad8d88e75adb46f8292bfe4a3615
Advaya24 <advaya24@yahoo.co.in>
2020-04-03 22:06:18 +0530

    Added content support

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     content = {'text': "Hello from Flask"}
-    return render_template('gocery/Index.html', text=content['text']), 200
+    return render_template('gocery/Index.html', content=content), 200
 
 
 if __name__ == '__main__':
