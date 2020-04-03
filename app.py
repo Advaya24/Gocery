91bfe5f34267e35eeef57f035135de8c041f12b5
Advaya24 <advaya24@yahoo.co.in>
2020-04-03 22:51:58 +0530

    Fixed got_location

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-import json
-
 from flask import Flask
 from flask import render_template
 from flask import request
-import requests
+import googlemaps
 
 app = Flask(__name__)
+
+gmaps = googlemaps.Client(key='')
 
 
 @app.route('/', methods=['GET'])
@@ -17,7 +17,8 @@ def hello_world():
 
 @app.route('/stores', methods=['POST'])
 def got_location():
-    return request.form['location']
+    locations = {'location': request.form['location']}
+    return render_template('gocery/Listing.html', locations=locations)
 
 
 if __name__ == '__main__':
